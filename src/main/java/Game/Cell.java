509c74b4d1530ba49c4b23e16a3eb62da6c8aa9b
pteacher <subfear@mail.ru>
2022-05-10 17:40:16 +0600

    Total remake of the game to discrete one. Tic-Tac-Toe example.

---
+++ src/main/java/Game/Cell.java
@@ -0,0 +1,60 @@
+/* Cell
+ *  Version information
+ *  5/10/2022
+ *  Copyright User
+ */
+
+package Game;
+
+public class Cell {
+    private int size;
+    private int x, y;
+
+    private STATE state;
+    private enum color {RED, GREEN, GREY};
+
+    public Cell(int size) {
+        this.size = size;
+        this.state = state.EMPTY;
+    }
+
+    public STATE getState() {
+        return state;
+    }
+
+    public Cell(int size, int x, int y) {
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.state = STATE.EMPTY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void changeState() {
+        if (this.state == STATE.EMPTY) {
+            this.state = STATE.O;
+        } else if (this.state == STATE.O) {
+            this.state = STATE.X;
+        } else {
+            this.state = STATE.EMPTY;
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (state == STATE.EMPTY) {
+            return "";
+        } else if (state == STATE.O) {
+            return "O";
+        } else {
+            return "X";
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,38 +4,75 @@
  *  Copyright User
  */
 
+import Game.Cell;
 import Game.Creature;
+import Game.STATE;
 import processing.core.PApplet;
+import processing.event.MouseEvent;
+
+// CELL BASED NON-ZERO PLAYER GAME
+// TIC-TAC-TOE, CHESS, CHECKERS, 2048 etc.
+// Using processing
+// Classes
+// Github with screenshots of your game
+// Penalties for plagiarism - 50 pts removal
+// Penalties for violating deadline - 1 pts each 1 hour
 
 public class Main extends PApplet {
     final int WIDTH = 800;
     final int HEIGHT = 600;
-    private boolean isStopped = true;
-    private Creature creature;
+    final int SIZE = 50;
+    final int ROWS = 5;
+    final int COLS = 5;
+    private Cell cells[] = new Cell[ROWS * COLS];
 
     @Override
     public void settings() {
-        fullScreen();
+        size(WIDTH, HEIGHT);
+
     }
 
     @Override
     public void setup() {
-        creature = new Creature(0, 0, 42, 100);
-        creature.teleport(1000, 1000);
+        textAlign(CENTER, CENTER);
+        textSize(42);
+        for (int i = 0; i < cells.length; i++) {
+            cells[i] = new Cell(SIZE, WIDTH / 2 + SIZE * (i % COLS) - (int) (SIZE * COLS / 2.0), HEIGHT / 2 + SIZE * (i / ROWS) - (int) (SIZE * ROWS / 2.0));
+        }
     }
 
     @Override
     public void draw() {
-        background(200);
-        // creature.moveTo(mouseX, mouseY);
-        if (!isStopped) {
-            creature.moveTo(0);
+        background(255);
+        for (int i = 0; i < cells.length; i++) {
+            if (cells[i].getState() == STATE.O) {
+                fill(200, 0, 0);
+            } else if (cells[i].getState() == STATE.X) {
+                fill(0, 200, 0);
+            } else {
+                fill(100, 100, 100);
+            }
+            rect(cells[i].getX(), cells[i].getY(), SIZE, SIZE);
+            fill(255);
+            text(cells[i].toString(), cells[i].getX() + SIZE / 2, cells[i].getY() + SIZE / 3);
+
         }
-        isStopped = !mousePressed;
-        int x = creature.getX();
-        int y = creature.getY();
-        int size = creature.getSize();
-        ellipse(x, y, size, size);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        for (int i = 0; i < cells.length; i++) {
+            if (event.getX() > cells[i].getX() &&
+                    event.getX() < cells[i].getX() + SIZE &&
+                    event.getY() > cells[i].getY() &&
+                    event.getY() < cells[i].getY() + SIZE) {
+                cells[i].changeState();
+            }
+        }
+        for (int i = 0; i < cells.length; i++) {
+            System.out.print(cells[i]);
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
